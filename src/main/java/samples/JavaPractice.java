00b1f7ce26ab0dc6c9905efa14c9069d7cdd523d
Akshaya Uppoor <akshaya.uppoor@gmail.com>
2018-09-12 18:33:06 +0530

    Pass by reference

--- src/main/java/samples/JavaPractice.java
+++ src/main/java/samples/JavaPractice.java
@@ -2,6 +2,35 @@ package samples;
 
 public class JavaPractice {
     public static void main(String[] args) {
-        
+        Person firstPerson = new Person(31, "Akshay");
+        Person secondPerson = new Person(37, "Amar");
+
+        fun(firstPerson, secondPerson);
+        System.out.println(firstPerson);
+        System.out.println(secondPerson);
+    }
+
+    public static void fun(Person x, Person y){
+        x = new Person (45, "Puneeth");
+        y = new Person (43, "Jayaram");
+    }
+}
+
+class Person
+{
+    int age;
+    String name;
+
+    public Person(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
     }
 }
