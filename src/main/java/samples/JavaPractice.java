d61ab2f795fbf98a533a36cd988deca7ead7d099
Akshaya Uppoor <akshaya.uppoor@gmail.com>
2018-09-12 12:09:15 +0530

    Committing code related to sudent marks

--- src/main/java/samples/JavaPractice.java
+++ src/main/java/samples/JavaPractice.java
@@ -2,6 +2,19 @@ package samples;
 
 public class JavaPractice {
     public static void main(String[] args) {
-        
+        String s1 = "This is a good book";
+        String s2 = new String("Core Java an integrated approach");
+        char[] arr = {'a', 'k', 's', 'h', 'a', 'y','a'};
+        String s3 = new String(arr);
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+        System.out.println(s3.length());
+        System.out.println(s3.concat(" ").concat(s1));
+        System.out.println(s3 + " "+ s1);
+        boolean x = s3.startsWith("A");
+        System.out.println(x);
+
+
     }
 }
